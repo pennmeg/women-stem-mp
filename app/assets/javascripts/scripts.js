39d5d5a719fe186664c4cc5574ce689d425a4e67
Megan Penn <penn.meg@gmail.com>
2017-11-21 11:19:41 -0500

    closes #2

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,45 +1,22 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
+  console.log("=== turbolinks:load ==");
   console.log("=== hello world! ===");
-    function showNav(){
-
-    };
-    $('#menuButton').on('click', function(){
-      console.log("--- menuButton ---");
-      $('nav').slideToggle('slow');
-    });
-    $('#scrollDown').on('click', function() {
-      scrollOverview();
-    });
-    $('#overviewButton').on('click', function(){
-      scrollOverview();
-    });
-    function scrollOverview(){
-      console.log("--- go to overview ---");
-      var offset = 20;
-      $('html, body').animate({
-       scrollTop: $("#overview").offset().top + offset}, 1500);
-    };
-    $('#countryButton').on('click', function(){
-      console.log("--- go to country ---");
-      var offset = 20;
-      $('html, body').animate({
-       scrollTop: $("#country").offset().top + offset}, 1500);
-    });
-    $('.count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 4000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
-// Add code to make navigation/header not show until you are past the home slide
-  $(function(){
-    // $('#world-map').vectorMap({
-    console.log("=== world map ===");
+  // ==== check pathname
+  var pathname = window.location.pathname;
+  console.log("pathname: ", pathname);
+  // === window conditional load map
+  if (pathname == "/country") {
+    console.log("=== PATHNAME: /country ===");
+    loadMap();
+  };
+  // $(function(){
+  //   $('#world-map').vectorMap({
+  //     map: 'world_mill'
+  //   });
+  // });
+  function loadMap(){
+  // $(function(){
+    console.log("=== loadMap ===");
     new jvm.Map({
       container: $('#world-map'),
       map: 'world_mill',
@@ -67,9 +44,51 @@ $(document).ready(function() {
             "US": 'blue' // USA
           }
         }],
-      regionsSelectable: true
-      // http://jvectormap.com/examples/regions-selection/
-      }
-    })
+      },
+      onRegionClick: function(event, code) {
+        var name = code;
+        console.log("-- click --");
+        console.log("Country: ", name);
+        if (name == "BR") {
+          console.log("Show BR Profile");
+        } else {
+          console.log("Please select a highlighted country")
+        }
+      },
+    });
+  };
+  $('#menuButton').on('click', function(){
+    console.log("--- menuButton ---");
+    $('nav').slideToggle('slow');
   });
+  $('#scrollDown').on('click', function() {
+    scrollOverview();
+  });
+  $('#overviewButton').on('click', function(){
+    scrollOverview();
+  });
+  function scrollOverview(){
+    console.log("--- go to overview ---");
+    var offset = 20;
+    $('html, body').animate({
+     scrollTop: $("#overview").offset().top + offset}, 1500);
+  };
+  $('#countryButton').on('click', function(){
+    console.log("--- go to country ---");
+    var offset = 20;
+    $('html, body').animate({
+     scrollTop: $("#country").offset().top + offset}, 1500);
+  });
+  $('.count').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 4000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+  });
+// Add code to make navigation/header not show until you are past the home slide
 });
