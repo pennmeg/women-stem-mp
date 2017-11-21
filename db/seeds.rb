465a0b4d3b30b7ce17969299d58aed36ed8daff1
Megan Penn <penn.meg@gmail.com>
2017-11-20 22:09:23 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Country.create ([ { name: "Brazil", country_code: "BR" }, { name: "China", country_code: "CN" },
+  { name: "Costa Rica", country_code: "CR" }, { name: "Egypt", country_code: "EG" }, { name: "Germany", country_code: "DE" },
+  { name: "Greece", country_code: "GR" }, { name: "India", country_code: "IN" }, { name: "Italy", country_code: "IT" },
+  { name: "Japan", country_code: "JP" }, { name: "Luxembourg", country_code: "LU" },
+  { name: "Myanmar", country_code: "MM" }, { name: "Netherlands", country_code: "NL" }, { name: "Nigeria", country_code: "NG" },
+  { name: "Saudi Arabia", country_code: "SA" }, { name: "United States of America", country_code: "US" } ])
