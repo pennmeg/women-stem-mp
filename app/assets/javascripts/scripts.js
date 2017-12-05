8880e926ca4cf34f31f8f768366d9946d5fbb3d0
Megan Penn <penn.meg@gmail.com>
2017-12-05 13:23:14 -0500

    adjusted flip time

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -75,7 +75,7 @@ $(document).on('turbolinks:load', function() {
     console.log("=== female toggle ===");
     setInterval(function(){
       $('#female').toggleClass('flip');
-    }, 4000);
+    }, 2000);
   };
   function homepageButtons(){
     console.log("=== homepageButtons ===");
